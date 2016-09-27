5d9c9755c4585dce7ad5d5929f438ae738e86d3c
Ronny Tomasetti <ronny.tomasetti@gmail.com>
2016-09-27 18:04:13 -0400

    Remove keys.js requirement.

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 var mysql = require('../node_modules/mysql');
-var keys = require('./keys');
+// var keys = require('./keys');
 
 var connection;
 
 if (process.env.JAWSDB_URL)
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 else {
-	connection = mysql.createConnection({
-		host     : keys.db.host,
-		user     : keys.db.user,
-		password : keys.db.password,
-		database : keys.db.database
-	});
+	// connection = mysql.createConnection({
+	// 	host     : keys.db.host,
+	// 	user     : keys.db.user,
+	// 	password : keys.db.password,
+	// 	database : keys.db.database
+	// });
 }
 
 connection.connect();
